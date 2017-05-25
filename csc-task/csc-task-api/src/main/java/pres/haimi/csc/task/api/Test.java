2106a495d0631efdc2eb1832da4dea5d15c579e1
Haimi <2514362803@qq.com>
2017-05-25 14:17:34 +0800

    add log4j

--- csc-task/csc-task-api/src/main/java/pres/haimi/csc/task/api/Test.java
+++ csc-task/csc-task-api/src/main/java/pres/haimi/csc/task/api/Test.java
@@ -2,6 +2,7 @@ package pres.haimi.csc.task.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import pres.haimi.csc.task.dao.task.CollegeTaskDao;
+import org.apache.log4j.Logger;
 
 /**
  * Created by HaimiZhou on 2017/5/22.
@@ -16,5 +17,11 @@ public class Test {
         System.out.println(collegeTaskDao.select(1));
         /*demoDao.add(new Demo(2,"hello"));*/
         System.out.println("Mybatis success!");
+    }
+    private static Logger logger= Logger.getLogger(Test.class);
+    public static void main(String[] args){
+        logger.debug("This is a debug message.");
+        logger.info("This is a info message.");
+        logger.error("This is a error message.");
     }
 }
